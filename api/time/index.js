8586b1028bdd1c88edf4fa6ec25b072ae041c8d0
Sean Matheson <sean@ctrlplusb.com>
2019-05-10 13:52:04 +0100

    Initial example

---
+++ api/time/index.js
@@ -0,0 +1,3 @@
+module.exports = (req, res) => {
+  res.end(new Date().toString());
+};
---
+++ www/src/App.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  const [serverDate, setServerDate] = useState();
+  useEffect(() => {
+    fetch("/api/time")
+      .then(response => response.text())
+      .then(text => setServerDate(text));
+  }, [setServerDate]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>The server time is {serverDate}</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
