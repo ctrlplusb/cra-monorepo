3051b09c67b3cfdd091b590eaebad85a8560a29b
Sean Matheson <sean@ctrlplusb.com>
2019-08-14 18:40:39 +0100

    Fixes typescript config

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,10 +1,6 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 
 module.exports = {
-  jest: function customiseJest(config) {
-    console.log(config);
-    return config;
-  },
   webpack: function customiseWebpack(config, env) {
     // Set up React Hot Loader
     config = rewireReactHotLoader(config, env);
--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,3 +1,9 @@
+/**
+ * I have done this "re-export" of the react-scripts jest configuration so that
+ * we are able to leverage Jest projects based execution from the root of the
+ * repo.
+ */
+
 const createJestConfig = require('react-scripts/scripts/utils/createJestConfig');
 
 const rootDir = __dirname;
